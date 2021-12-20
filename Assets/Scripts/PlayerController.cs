c25723431a268e5aa5cac2547ca1e0497d480430
Kerri Newcomer <newco098@umn.edu>
2021-12-19 18:11:31 -0600

    Environment setup 2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,14 +6,26 @@ public class PlayerController : MonoBehaviour
 {
     private int speed = 10;
 
+    public bool started;
+
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.RightArrow)){
-            gameObject.transform.Translate(Vector3.right * speed * Time.deltaTime);
+        if(started){
+            if(Input.GetKey(KeyCode.RightArrow)){
+                gameObject.transform.Translate(Vector3.right * speed * Time.deltaTime);
+            }
+            if(Input.GetKey(KeyCode.LeftArrow)){
+                gameObject.transform.Translate(Vector3.left * speed * Time.deltaTime);
+            }
         }
-        if(Input.GetKey(KeyCode.LeftArrow)){
-            gameObject.transform.Translate(Vector3.left * speed * Time.deltaTime);
-        }
+    }
+
+    public void StartGame(){
+        started = true;
+    }
+
+    public void MoveToStart(){
+        transform.position = new Vector3(0, 20.5f, 0);
     }
 }
